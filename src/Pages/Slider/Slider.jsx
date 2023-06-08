e20a9c3398fce3a7691c6e4ec3c309c1dd2f45b9
safatsaat2 <sakibsafat47@gmail.com>
2023-06-08 23:44:38 +0600

    slider added

--- src/Pages/Slider/Slider.jsx
+++ src/Pages/Slider/Slider.jsx
@@ -5,7 +5,7 @@ const Slider = () => {
         <div>
             <div className="carousel w-full h-[500px]">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/mCWDr1x/image.png" className="w-full" />
+                    <img src="https://i.ibb.co/mCWDr1x/image.png" className="w-full object-cover" />
                     <div className="absolute flex flex-col">
                         <h1 className="lg:text-5xl font-semibold w-1/2 my-16 mx-28"><span className="text-[#20A8CC]">Unleash the beauty </span> through the lens: Capturing lifes moments with passion.</h1>
                         <div className="lg:w-1/3 mx-28 grid grid-cols-2 gap-6">
@@ -43,6 +43,10 @@ const Slider = () => {
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
                     <img src="https://i.ibb.co/QvRnsqL/image.png" className="w-full" />
+                    
+                    <div className="absolute left-32 my-20">
+                        <h1 className="text-6xl text-center font-semibold">Capturing Moments, Preserving Memories.</h1>
+                    </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
