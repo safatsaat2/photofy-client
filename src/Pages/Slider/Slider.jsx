ae0248e7c13564d9fb4f6f10b662ce67e3fad39f
safatsaat2 <sakibsafat47@gmail.com>
2023-06-10 00:48:30 +0600

    slide ready

--- src/Pages/Slider/Slider.jsx
+++ src/Pages/Slider/Slider.jsx
@@ -5,7 +5,7 @@ const Slider = () => {
         <div>
             <div className="carousel w-full h-[500px]">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/mCWDr1x/image.png" className="w-full object-cover" />
+                    <img src="https://i.ibb.co/mCWDr1x/image.png" className="w-full  opacity-80 object-cover" />
                     <div className="absolute flex flex-col">
                         <h1 className="lg:text-5xl font-semibold w-1/2 my-16 mx-28"><span className="text-[#20A8CC]">Unleash the beauty </span> through the lens: Capturing lifes moments with passion.</h1>
                         <div className="lg:w-1/3 mx-28 grid grid-cols-2 gap-6">
@@ -42,8 +42,8 @@ const Slider = () => {
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/QvRnsqL/image.png" className="w-full" />
-                    
+                    <img src="https://i.ibb.co/QvRnsqL/image.png" className="w-full opacity-90" />
+
                     <div className="absolute left-32 my-20">
                         <h1 className="text-6xl text-center font-semibold">Capturing Moments, Preserving Memories.</h1>
                     </div>
@@ -53,14 +53,21 @@ const Slider = () => {
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                    <img src="https://i.ibb.co/9T7894R/image.png" className="w-full opacity-90" />
+                    <div className="absolute my-20">
+                        <h1 className="text-6xl text-white text-border text-center font-semibold">Capturing Life`s Beauty: Explore the World of Photography.</h1>
+                    </div>
+
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
-                    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                    <img src="https://i.ibb.co/B3FH2yc/image.png" className="w-full opacity-90" />
+                    <div className="absolute my-20">
+                        <h1 className="text-6xl text-white text-border text-center font-semibold">Through the Lens: Explore the Artistry of Photography</h1>
+                    </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
